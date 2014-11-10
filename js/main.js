f19bb61c907c9609b388925bcb2e48becf44963b
amindunited <buckley.robin@gmail.com>
2014-11-11 08:54:16 +1100

    Updates

--- js/main.js
+++ js/main.js
@@ -1,2 +1,41 @@
 /** Copyright Robin Buckley. 2014 **/
 //Main.js
+$(function(){
+	var $cs = $('.styled').customSelect();
+});
+
+$(function(){
+    $('input').iCheck({
+        checkboxClass: 'icheckbox_square-grey',
+        radioClass: 'iradio_square-grey',
+        increaseArea: '20%' // optional
+    });
+});
+
+//On page / element load:
+//...if the number of elements is dynamic
+
+// the width of the 'scroller' should be:
+// the width of the elements * the number of elements
+$(function(){
+    
+    var example_1_li_widths = $('#example_1 > .scroller > ul > li').outerWidth();
+    var example_1_num_elements = $('#example_1 > .scroller > ul > li').length;
+    $('#example_1 > .scroller').width( example_1_li_widths * example_1_num_elements );
+
+});
+
+//
+// create the scroller
+$(function(){
+    example_1 = new IScroll('#example_1', {
+        scrollX: true,//Set to only horiziontal scroll
+        scrollY: false,//turn of vertical scroll
+        snap: false,//'li',//'snap' scrolling to element widths (values: false | true or element tagName)
+        mouseWheel: true,//Enable disable mouse wheel
+        scrollbars: false,//show hide scrollbars
+        bounce: true,//enable 'bouce' animation
+        preventDefault: true,//prevent default scroll events from triggering 
+        
+    });
+});
